8a66f4baeeed4a7138daa9b5884acbce74a563ec
Muskansaini786 <muskansaini455gmail.com>
2024-07-05 21:50:32 +0530

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,13 +19,14 @@ const Contact = () => {
           <ContactForm />
         </div>
       </div>
-      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-        {/* Reviws from Other Learner */}
-        <h1 className="text-center text-4xl font-semibold mt-8">
-          Reviews from other learners
-        </h1>
-        <ReviewSlider />
+
+      <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+
+            <h2 className='text-center text-4xl font-semobold mt-10'>review from Other Learners</h2>
+            {/* Review Slider here */}
+            <ReviewSlider />
       </div>
+
       <Footer />
     </div>
   )
