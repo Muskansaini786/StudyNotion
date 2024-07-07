6948f9681b3ec6ded4fc4384037f30914eb0ada3
Muskansaini786 <muskansaini455gmail.com>
2024-07-07 12:34:00 +0530

    commit

--- Server/routes/Course.js
+++ Server/routes/Course.js
@@ -88,7 +88,6 @@ router.post("/updateCourseProgress", auth, isStudent, updateCourseProgress);
 //                                      Category routes (Only by Admin)
 // ********************************************************************************************************
 // Category can Only be Created by Admin
-// TODO: Put IsAdmin Middleware here
 router.post("/createCategory", auth, isAdmin, createCategory)
 router.get("/showAllCategories", showAllCategories)
 router.post("/getCategoryPageDetails", categoryPageDetails)
